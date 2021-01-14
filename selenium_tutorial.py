4827af3bdf4a3107ce227c77f9a9529593ff791e
Jaewon Han <38302837+ellynhan@users.noreply.github.com>
2021-01-14 22:48:11 +0900

    just for warming up for scrapping

---
+++ selenium_tutorial.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('headless')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(executable_path="~PATH~", options=chrome_options)
+driver.get("https://naver.com/")
+a = driver.find_elements_by_tag_name("a")
+count =0
+for element in a:
+    print(element.get_attribute('innerHTML'))
+    print(element.get_attribute('href'))
+    count=count+1
+    if (count>10) :
+        break
+
+driver.quit()
